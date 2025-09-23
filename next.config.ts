d394fa4f82dffc015929adc1b98199449c23ea11
bbicoon <owlboo114@gmail.com>
2025-09-23 16:52:11 +0900

    현재까지 작업물의 개선작업

--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,6 @@ const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname, // 워크스페이스 루트 경고 해결
   images: {
     unoptimized: true, // 정적 내보내기와 호환되도록 이미지 최적화 비활성화
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-        port: "",
-        pathname: "/**",
-      },
-    ],
   },
 };
 
